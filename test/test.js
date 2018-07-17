3cdcc8d2ac0132cfc9b9b31d58467a93aeeaeada
briantam23 <briantam23@gmail.com>
2018-07-17 19:24:06 -0400

    added tests

--- test/test.js
+++ test/test.js
@@ -1,19 +1,27 @@
 const expect = require('chai').expect;
 const {convertToFfromC, convertToCfromF} = require('../index');
 
-describe("convertToFfromC", () => {
-    it("exists", () => {
-        expect(convertToFfromC).to.be.ok;
+describe("converter", () => {
+    describe("convertToFfromC", () => {
+        it("exists", () => {
+            expect(convertToFfromC).to.be.ok;
+        })
+        it("0 degrees Celcius should be 32 degrees F", () => {
+            expect(convertToFfromC(0)).to.equal(32);
+        })
+        it("100 degrees Celcius should be 212 degrees F", () => {
+            expect(convertToFfromC(100)).to.equal(212);
+        })
     })
-    it("converts", () => {
-        expect(convertToFfromC(0)).to.equal(32);
-    })
-})
-describe("convertToCfromF", () => {
-    it("exists", () => {
-        expect(convertToCfromF).to.be.ok;
-    })
-    it("converts", () => {
-        expect(convertToCfromF(32)).to.equal(0);
+    describe("convertToCfromF", () => {
+        it("exists", () => {
+            expect(convertToCfromF).to.be.ok;
+        })
+        it("32 degrees F should be 0 degrees Celcius", () => {
+            expect(convertToCfromF(32)).to.equal(0);
+        })
+        it("212 degrees F should be 100 degrees Celcius", () => {
+            expect(convertToCfromF(212)).to.equal(100);
+        })
     })
 })
